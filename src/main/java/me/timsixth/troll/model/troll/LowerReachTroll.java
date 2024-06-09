f0794d29e9edfa2031513049e9e26d225b607753
Diamond Iq <diamentowyiq@gmail.com>
2024-06-09 13:33:47 +0200

    Added Lower_Reach Troll, No_Internet Troll and Warden_Sound Troll

---
+++ src/main/java/me/timsixth/troll/model/troll/LowerReachTroll.java
@@ -0,0 +1,34 @@
+package me.timsixth.troll.model.troll;
+
+import lombok.RequiredArgsConstructor;
+import me.timsixth.troll.TrollPlugin;
+import me.timsixth.troll.config.Messages;
+import me.timsixth.troll.model.Troll;
+import me.timsixth.troll.model.TrolledUserProperties;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+@RequiredArgsConstructor
+public class LowerReachTroll implements Troll {
+
+    private final Messages messages;
+
+
+    @Override
+    public void executeTroll(Player other, Player sender, TrolledUserProperties userProperties) {
+        if (!userProperties.isLoweredReach()){
+            userProperties.setLoweredReach(true);
+            sender.sendMessage(messages.getLowerReachEnabled());
+        } else {
+            userProperties.setLoweredReach(false);
+            sender.sendMessage(messages.getLowerReachDisabled());
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "LOWER_REACH";
+    }
+}
